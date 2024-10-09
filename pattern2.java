1b88e7c1860ad3d37a11883061a04ad2d33f4f79
antonio-fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2024-10-08 23:35:03 -0300

    Padrões de print
    treino de Java básico fazendo padrões de output que possivelmente podem ser úteis no futuro.

---
+++ pattern2.java
@@ -0,0 +1,17 @@
+package printnicepattern;
+
+ class pattern2 
+{  
+    public static void main(String[] args)  
+    {  
+        int n = 4;  
+        for(int i = 0 ; i <= n ; i++)  
+        {  
+        for(int j = 0 ; j <= i ; j++)  
+        {  
+            System.out.print(" "+(char)(65 + i));  
+        }  
+            System.out.println("");  
+        }  
+    }  
+}  
---
+++ pattern3.java
@@ -0,0 +1,18 @@
+package printnicepattern;
+
+class pattern3  {
+  
+   public static void main(String[] args)  
+   {  
+   int i,j, n=7;  
+   System.out.println("Right angle triangle");  
+   for(i=1;i<n;i++)  
+   {  
+   for(j=1;j<=i;j++)  
+   {  
+       System.out.print(" *");  
+   }  
+       System.out.println("");  
+   }  
+   }
+}
