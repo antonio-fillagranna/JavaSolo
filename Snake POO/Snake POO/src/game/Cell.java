ed11b787c3a6a6bd7b614e8dd8f9a8f1ccd62c17
antonio-fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2024-10-26 23:25:34 -0300

    Another snake game
    This time using concepts of POO, and no front end implemented (yet).

---
+++ Snake POO/Snake POO/src/game/Cell.java
@@ -0,0 +1,22 @@
+public class Cell {
+
+	private final int row, col;
+	private CellType cellType;
+
+	public Cell(int row, int col)
+	{
+		this.row = row;
+		this.col = col;
+	}
+
+	public CellType getCellType() { return cellType; }
+
+	public void setCellType(CellType cellType)
+	{
+		this.cellType = cellType;
+	}
+
+	public int getRow() { return row; }
+
+	public int getCol() { return col; }
+}
---
+++ Snake POO/Snake POO/src/game/CellType.java
@@ -0,0 +1,6 @@
+public enum CellType {
+
+	EMPTY,
+	FOOD,
+	SNAKE_NODE;
+}
